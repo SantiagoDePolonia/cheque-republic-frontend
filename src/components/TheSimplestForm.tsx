e7fd1857668562712b80ba78f7c751539fedeeb6
Jakub A. W <jakubwasek@gmail.com>
2023-06-06 14:07:35 +0200

    feat: add minting steps

--- src/components/TheSimplestForm.tsx
+++ src/components/TheSimplestForm.tsx
@@ -1,9 +1,12 @@
 import Select, { OnChangeValue } from 'react-select'
-import { NETWORKS_OPTIONS } from '../constants';
+import { NETWORKS_OPTIONS, WEB3_STORAGE_TOKEN } from '../constants';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import InputWrapper from './InputWrapper';
+// @ts-ignore
+import { Web3Storage } from 'web3.storage'
 
-import { Erc721Standard, IPFSWrapper, Web3Connection } from '@taikai/dappkit';
+import { Erc721Standard, Web3Connection } from '@taikai/dappkit';
+
 
 type CREATING_STATE_STATUS = 'none' | 'connecting' | 'networkValidate' | 'creating_cid' | 'creating' | 'minted';
 interface NetworkOption {
@@ -15,9 +18,11 @@ interface NetworkOption {
 function TheSimplestForm() {
   const [network, setNetwork] = useState<NetworkOption>(NETWORKS_OPTIONS[0]);
   const [name, setName] = useState('');
+  const [file, setFile] = useState<File>();
+  const [cid, setCID] = useState<string>();
+
   const [creatingState, setCreatingState ] = useState<CREATING_STATE_STATUS>('none');
   const web3Connection = useRef<Web3Connection>();
-
   const onNetworkChange = useCallback((
     newValue: OnChangeValue<NetworkOption, false>,
   ) => {
@@ -60,14 +65,23 @@ function TheSimplestForm() {
 
     receipt = await myNFT.mint(await web3Connection.current.getAddress(), 1);
     console.log('Minted!', receipt);
-    setCreatingState('minted');
   }, []);
+
+  const onCreatingCID = useCallback(async () => {
+    const storage = new Web3Storage({ token: WEB3_STORAGE_TOKEN })
+
+    console.log(`Uploading file`)
+    const cid = await storage.put([file], {wrapWithDirectory: false})
+
+    console.log('Content added with CID:', cid)
+    setCID(cid);
+    setCreatingState('creating');
+  }, [file]);
 
   useEffect(() => {
     if (creatingState === 'connecting') {
       web3Connection.current = new Web3Connection({ 
         web3Host: 'https://mainnet.infura.io/v3/3b53db43bd164953a1b04e00eb40f0b2',
-        /* privateKey: '' */
       });
 
       web3Connection.current.start();
@@ -83,29 +97,35 @@ function TheSimplestForm() {
         setCreatingState('creating_cid');
       });
     } else if (creatingState === 'creating_cid') {
-      new IPFSWrapper()
+      onCreatingCID().then(() => {
+        setCreatingState('creating_cid');
+      });
     } else if (creatingState === 'creating') {
 
       onCreatingNFT();
     } else if (creatingState === 'minted') {
       console.log('minted');
     }
-  }, [creatingState, onNetworkChange, onNetworkValidate, onCreatingNFT])
+  }, [creatingState, onNetworkChange, onNetworkValidate, onCreatingNFT, onCreatingCID])
   const handleOnClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
       e.preventDefault();
       setCreatingState('connecting');
   }, []);
-
+  const handleOnFileChange = useCallback((e)=> {
+    if(e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  },[]);
   return <form>
     <Select options={NETWORKS_OPTIONS} value={network} onChange={onNetworkChange} />
     <InputWrapper>
       <input value={name} onChange={e => setName(e.target.value)} name='name'  type='text' />
     </InputWrapper>
     <InputWrapper>
-      <input name='file' type='file' />
+      <input name='file' type='file' onChange={handleOnFileChange} />
     </InputWrapper>
 
-    <button type='submit' onClick={handleOnClick} disabled={creatingState !== 'none'}>
+    <button type='submit' onClick={handleOnClick} disabled={creatingState !== 'none' || !name || !file || !network }>
       CREATE YOUR NFT
     </button>
   </form>
