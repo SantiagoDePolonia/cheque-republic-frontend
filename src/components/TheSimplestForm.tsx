38516536f4b8b3349292b8fc1e06ef81fef8bfe4
Jakub A. W <jakubwasek@gmail.com>
2023-06-07 08:35:49 +0200

    feat: add changes

--- src/components/TheSimplestForm.tsx
+++ src/components/TheSimplestForm.tsx
@@ -15,11 +15,20 @@ interface NetworkOption {
   chainId: string;
 }
 
+interface ERC721Metadata {
+  name: string;
+  description: string;
+  image: string;
+}
+
 function TheSimplestForm() {
   const [network, setNetwork] = useState<NetworkOption>(NETWORKS_OPTIONS[0]);
   const [name, setName] = useState('');
   const [file, setFile] = useState<File>();
   const [cid, setCID] = useState<string>();
+  const [cidMetadata, setCIDMetadata] = useState<string>();
+
+  const [receipt, setReceipt] = useState<any>();
 
   const [creatingState, setCreatingState ] = useState<CREATING_STATE_STATUS>('none');
   const web3Connection = useRef<Web3Connection>();
@@ -49,24 +58,29 @@ function TheSimplestForm() {
     const deployer = new Erc721Standard({
       web3CustomProvider: window.ethereum,
     });
-
     deployer.connection.start();
     deployer.loadAbi();
 
-    let receipt;
+    const receiptDeploy = await deployer.deployJsonAbi('My NFT Name', '$MyNftSymbol');;
 
-    receipt = await deployer.deployJsonAbi('My NFT Name', '$MyNftSymbol');
-    console.log('deployed!', receipt);
+    setReceipt(receiptDeploy)
+
+    console.log('deployed!', receiptDeploy);
 
     /* Using your NFT after deployment */
-    const myNFT = new Erc721Standard(deployer.connection, receipt.contractAddress);
+    const myNFT = new Erc721Standard(deployer.connection, receiptDeploy.contractAddress);
 
     await myNFT.loadContract();
 
-    receipt = await myNFT.mint(await web3Connection.current.getAddress(), 1);
-    console.log('Minted!', receipt);
-  }, []);
+    const receipt = await myNFT.mint(await web3Connection.current.getAddress(), 1);
 
+    console.log(cidMetadata)
+    if(cidMetadata)
+      await deployer.setTokenURI(1, 'https://ipfs.io/ipfs/'+cidMetadata )
+
+    console.log('Minted!', receipt);
+  }, [cid, cidMetadata]);
+  
   const onCreatingCID = useCallback(async () => {
     const storage = new Web3Storage({ token: WEB3_STORAGE_TOKEN })
 
@@ -75,6 +89,22 @@ function TheSimplestForm() {
 
     console.log('Content added with CID:', cid)
     setCID(cid);
+    console.log("cid",cid)
+
+    const metadataJson: ERC721Metadata = {
+      name: name,
+      description: name,
+      image: 'https://ipfs.io/ipfs/'+cid
+    }
+
+    const fileMetadata = new Blob([JSON.stringify(metadataJson)], { type : 'plain/text' });
+    const fileMetadataFile = new File([fileMetadata], 'metadata.json');
+
+    const cidMetadata = await storage.put([fileMetadataFile], {wrapWithDirectory: false})
+    console.log("cidMetadata",cidMetadata)
+
+    setCIDMetadata(cidMetadata);
+
     setCreatingState('creating');
   }, [file]);
 
@@ -98,11 +128,12 @@ function TheSimplestForm() {
       });
     } else if (creatingState === 'creating_cid') {
       onCreatingCID().then(() => {
-        setCreatingState('creating_cid');
+        setCreatingState('creating');
       });
     } else if (creatingState === 'creating') {
-
-      onCreatingNFT();
+      onCreatingNFT().then(() => {
+        setCreatingState('minted');
+      });
     } else if (creatingState === 'minted') {
       console.log('minted');
     }
@@ -111,11 +142,32 @@ function TheSimplestForm() {
       e.preventDefault();
       setCreatingState('connecting');
   }, []);
-  const handleOnFileChange = useCallback((e)=> {
-    if(e.target.files) {
-      setFile(e.target.files[0]);
+  const handleOnFileChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=> {
+    if((e.target as HTMLInputElement).files?.[0]) {
+      setFile((e.target as HTMLInputElement)?.files?.[0]);
     }
   },[]);
+
+  if(creatingState === 'minted') {
+    const blockchainExplorer = 'https://sepolia.etherscan.io/token/' + receipt.contractAddress;
+    return (<div className='congratulations'>
+      <h2>Your NFT has been minted!</h2>
+      <p className='status'>
+        NFT Address: {receipt.contractAddress}<br />
+        Blockchain Explorer: <a href={blockchainExplorer} target='_blank' rel='noreferrer'>
+          {blockchainExplorer}
+        </a>
+      </p>
+    </div>)
+  }
+
+  if(creatingState !== 'none') {
+    return (<>
+      <div className='spinner'></div>
+      <div className='status'>{creatingState}...</div>
+    </>)
+  }
+
   return <form>
     <Select options={NETWORKS_OPTIONS} value={network} onChange={onNetworkChange} />
     <InputWrapper>
